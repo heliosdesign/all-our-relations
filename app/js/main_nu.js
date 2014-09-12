d686324b2d3458d999638a063f0395afb89adaf9
dazu1990 <adrian.davilazuniga@gmail.com>
2014-09-12 15:42:07 -0400

    merging

--- app/js/main_nu.js
+++ app/js/main_nu.js
@@ -186,6 +186,10 @@ $(function(){
 				  		status = "404"
 				  		console.log("404")
 						$( "#contentPanel" ).css('display','none')
+			  		}else{
+			  			console.log("show partial " + _partial)
+			  			$('.ancestor').fadeIn()
+			  			$( "#contentPanel" ).fadeIn()
 			  		}
 
 			  		console.log("loaded partial " + _partial)
@@ -206,11 +210,11 @@ $(function(){
 					
 					
 
-			  		if(_status == "200"){
-			  			console.log("show partial " + _partial)
-			  			$('.ancestor').fadeIn()
-			  			$( "#contentPanel" ).fadeIn()
-			  		}
+			  		// if(_status == "200"){
+			  		// 	console.log("show partial " + _partial)
+			  		// 	$('.ancestor').fadeIn()
+			  		// 	$( "#contentPanel" ).fadeIn()
+			  		// }
 			  	
 			});			
 			})
@@ -485,51 +489,104 @@ $(function(){
 
 /////////EVENTS and LOCATION HASHES
 
+		
+
 		var AORloc = window.location.hash;
 
-		console.log("hello")
+		// $(window).on('hashchange', function() {
+		// 	var AORloc = window.location.hash;
 
-		if (AORloc) {
+		// 	if (AORloc) {
 
-			// $('#parchment-scrim').css('display','none')
-			// $('#all-container').css('display','block')
-			// $('#bg-video').css('display','none')
-			switch (AORloc) {
+		// 		// $('#parchment-scrim').css('display','none')
+		// 		// $('#all-container').css('display','block')
+		// 		// $('#bg-video').css('display','none')
+		// 		switch (AORloc) {
 
-				case "#timeline":
-					$('.btn-timeline').addClass('btn-active');
+		// 			case "#timeline":
+		// 				$('.btn-timeline').addClass('btn-active');
 
-					console.log("should build timeline")
+		// 				console.log("should build timeline")
 
-					buildTimeline();
-				break;
+		// 				buildTimeline();
+		// 			break;
 
-				case "#intro":
-					$('.btn-intro').addClass('btn-active');
-					buildIntro();
-				break;	
+		// 			case "#intro":
+		// 				$('.btn-intro').addClass('btn-active');
+		// 				buildIntro();
+		// 			break;	
 
-				case "#resource":
-					$('.btn-resources').addClass('btn-active');
-					buildResources();
-				break;	
+		// 			case "#resource":
+		// 				$('.btn-resources').addClass('btn-active');
+		// 				buildResources();
+		// 			break;	
 
-				case "#about":
-					$('.btn-about').addClass('btn-active');
-					buildAbout();
-				break;	
+		// 			case "#about":
+		// 				$('.btn-about').addClass('btn-active');
+		// 				buildAbout();
+		// 			break;	
 
-				case "#shows":
-					$('.btn-about').addClass('btn-active');
-					buildShow();
-				break;	
+		// 			case "#shows":
+		// 				$('.btn-about').addClass('btn-active');
+		// 				buildShow();
+		// 			break;	
 
-			}	
+		// 		}	
 
-		} else {
-			$('.btn-intro').addClass('btn-active')
-			buildIntro()
-		}
+		// 	} else {
+		// 		$('.btn-intro').addClass('btn-active')
+		// 		buildIntro()
+		// 	}
+		//   console.log(AORloc)
+		// });
+		console.log(AORloc)
+
+			if (AORloc) {
+
+				// $('#parchment-scrim').css('display','none')
+				// $('#all-container').css('display','block')
+				// $('#bg-video').css('display','none')
+				switch (AORloc) {
+
+					case "#timeline":
+						$('.btn-timeline').addClass('btn-active');
+
+						console.log("should build timeline")
+
+						buildTimeline();
+					break;
+
+					case "#intro":
+						$('.btn-intro').addClass('btn-active');
+						buildIntro();
+					break;	
+
+					case "#resource":
+						$('.btn-resources').addClass('btn-active');
+						buildResources();
+					break;	
+
+					case "#about":
+						$('.btn-about').addClass('btn-active');
+						buildAbout();
+					break;	
+
+					case "#shows":
+						$('.btn-about').addClass('btn-active');
+						buildShow();
+					break;	
+
+				}	
+				// $('#parchment-scrim').css('display','none')
+				// $('#all-container').css('display','block')
+				// $('#bg-video').css('display','none')
+
+			} else {
+				$('.btn-intro').addClass('btn-active')
+				buildIntro()
+			}
+
+		
 
 
 		$( "#contentPanel" ).hover(
